096dc55705c677223dae292933c10349c51f1a02
Theo Carnavos <tcarnavos@gmail.com>
2016-01-13 09:19:27 -0600

    initializing version 2 branch with js file

---
+++ songs.js
@@ -0,0 +1,7 @@
+var songs = [];
+
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
